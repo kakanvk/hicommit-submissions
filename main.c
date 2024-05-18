b1a980e82d97587c15a931386f540f502c770fa9
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2024-05-18 16:45:43 +0700

    Test InCorrect Answer

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 int is_prime(int n) {
-    if (n <= 1) return 0;
+    if (n <= 1) return 1;
     if (n <= 3) return 1;
     if (n % 2 == 0 || n % 3 == 0) return 0;
     for (int i = 5; i * i <= n; i += 6) {

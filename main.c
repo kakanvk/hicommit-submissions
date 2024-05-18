d723899244ae1118953908036be5a775113a02c2
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2024-05-18 11:00:09 +0700

    Submit code at 10:57:53 18/5/2024

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int is_prime(int n) {
     if (n <= 1) return 0;
-    if (n <= 3) return 1;
+    if (n <= 3) return 0;
     if (n % 2 == 0 || n % 3 == 0) return 0;
     for (int i = 5; i * i <= n; i += 6) {
         if (n % i == 0 || n % (i + 2) == 0) return 0;
@@ -18,7 +18,6 @@ int main() {
         printf("Yes\n");
     } else {
         printf("No\n");
-      // test
     }
     return 0;
 }

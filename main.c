012a7e5453e9d276d0c4db13929c88bccd9e6c44
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2024-05-18 17:23:29 +0700

    Test Realtime Check

--- main.c
+++ main.c
@@ -7,17 +7,6 @@ int is_prime(int n) {
     if (n % 2 == 0 || n % 3 == 0) return 0;
     for (int i = 5; i * i <= n; i += 6) {
         if (n % i == 0 || n % (i + 2) == 0) return 0;
-
-    
-          
-            
-    
-
-          
-          Expand Down
-    
-    
-  
     }
     return 1;
 }

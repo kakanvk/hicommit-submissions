1a03934c86294f3718b793881839cb965f9d58e7
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2024-05-19 15:10:33 +0700

    Test

--- main.c
+++ main.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
-#include <math.h>
 
-int is_prime(int n) {
-    if (n <= 1) return 0;
-    if (n <= 3) return 1;
-    if (n % 2 == 0 || n % 3 == 0) return 0;
-    for (int i = 5; i * i <= n; i += 6) {
-        if (n % i == 0 || n % (i + 2) == 0) return 0;
+int a;
+
+int soNguyenTo(int soA)
+{
+    if (soA < 2)    
+        return 0;
+
+    for (int i = 2; i <= sqrt((float)soA); i ++)
+    {
+        if (soA%i==0)
+        {
+            return 0;
+        }
     }
     return 1;
 }
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    if (is_prime(n)) {
-        printf("Yes");
-    } else {
-        printf("No");
-    }
+
+    scanf("%d", &a);
+
+    if(soNguyenTo(a))
+      printf("Yes");
+    else printf("No");
+    
     return 0;
 }

7753bba5709c50ec68756a4195babcb43de40187
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2024-05-19 20:27:46 +0700

    Submit Uoc So

--- main.cpp
+++ main.cpp
@@ -2,28 +2,64 @@
 
 using namespace std;
 
-bool isPrime(int n) {
-    if (n <= 1) return false;
-    if (n <= 3) return true;
-    if (n % 2 == 0 || n % 3 == 0) return false;
-    for (int i = 5; i * i <= n; i += 6) {
-        if (n % i == 0 || n % (i + 2) == 0) return false;
-    }
-    return true;
+//0 1 1 2 3 5 8 13 21 34 55 89 144 233
+//0 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+
+long long multi (long long a, long long b, long long m) {
+	if (b==0) return 0;
+	long long t = multi (a, b/2, m) % m;
+	if (b % 2 == 1) {
+		return ((t+t)%m+a%m)%m;
+	}
+	else {
+		return (t+t)%m;
+	}
 }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int num;
-    cin >> num;
-
-    if (isPrime(num)) {
-        cout << "Yes\n";
-    } else {
-        cout << "No\n";
+void matrix_multiplication(long long A[2][2], long long B[2][2], long long M) {
+  long long C[2][2];
+  for (long long i = 0; i < 2; i++) {
+    for (long long j = 0; j < 2; j++) {
+      C[i][j] = 0;
+      for (long long k = 0; k < 2; k++) {
+        C[i][j] += multi (A[i][k], B[k][j], M);
+        C[i][j]%=M;
+      }
     }
+  }
+  for (long long i = 0; i < 2; i++) {
+    for (long long j = 0; j < 2; j++) {
+      A[i][j] = C[i][j];
+    }
+  }
+}
 
-    return 0;
+long long fib(long long n, long long m) {
+  if (n <= 1) {
+    return n;
+  }
+  long long F[2][2] = {{1, 1}, {1, 0}};
+  long long result[2][2] = {{1, 0}, {0, 1}};
+  while (n > 0) {
+    if (n % 2 == 1) {
+      matrix_multiplication(result, F, m);
+    }
+    matrix_multiplication(F, F, m);
+    n /= 2;
+  }
+  return result[0][1];
+}
+
+void solve () {
+	long long a, b, m, k;
+	cin >> a >> b >> m;
+	k = __gcd(a, b);
+	cout << fib(k, m) << endl;
+}
+
+int main () {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);  cout.tie(0);
+	solve();
+	return 0;
 }
